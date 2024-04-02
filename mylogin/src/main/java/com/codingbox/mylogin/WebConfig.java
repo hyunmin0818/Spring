30e1b8b384eecdd2a204df95ffd9bf313eefdfe9
임현민 <120604k@gmail.com>
2024-04-02 15:57:55 +0900

    종료

--- mylogin/src/main/java/com/codingbox/mylogin/WebConfig.java
+++ mylogin/src/main/java/com/codingbox/mylogin/WebConfig.java
@@ -1,18 +1,31 @@
 package com.codingbox.mylogin;
 
 import com.codingbox.mylogin.filter.LogFilter;
+import com.codingbox.mylogin.filter.LoginCheckFilter;
 import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebConfig {
+
+    @Bean
     public FilterRegistrationBean logFilter(){
     FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<Filter>();
     // 내가 만든 LogFilter를 넣어준다.
     filterFilterRegistrationBean.setFilter(new LogFilter());
     filterFilterRegistrationBean.setOrder(1);
     // 모든 url에 다 적용된다.
+    filterFilterRegistrationBean.addUrlPatterns("/*");
+    return filterFilterRegistrationBean;
+    }
+    @Bean
+    public FilterRegistrationBean LoginCheckFilter(){
+    FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<Filter>();
+    filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
+    filterFilterRegistrationBean.setOrder(2);
     filterFilterRegistrationBean.addUrlPatterns("/*");
     return filterFilterRegistrationBean;
     }
--- mylogin/src/main/java/com/codingbox/mylogin/domain/login/LoginController.java
+++ mylogin/src/main/java/com/codingbox/mylogin/domain/login/LoginController.java
@@ -22,51 +22,50 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
-	
+
 	private final LoginService loginService;
-	
+
 	@GetMapping("/login")
-	public String loginForm(@ModelAttribute("loginForm")LoginForm loginForm) {
+	public String loginForm(@ModelAttribute("loginForm") LoginForm loginForm) {
 		return "login/loginForm";
 	}
-	
+
 	/*
 	 * Model : forward 방식
 	 * RedirectAttributes : redirect 방식
 	 */
 //	@PostMapping("/login")
 	public String loginv2(@ModelAttribute LoginForm form,
-			Model model, RedirectAttributes reAttributes,
-			HttpServletResponse response) {
-		Member loginMember = 
-			loginService.login(form.getLoginId(), form.getPassword());
-		if( loginMember == null ) {
+						  Model model, RedirectAttributes reAttributes,
+						  HttpServletResponse response) {
+		Member loginMember =
+				loginService.login(form.getLoginId(), form.getPassword());
+		if (loginMember == null) {
 			// 로그인 실패
 			model.addAttribute("msg", "로그인 실패");
 			return "login/loginForm";
 		}
-		
+
 		// 로그인 성공
 		// 쿠키에다가 저장
-		Cookie idCookie = new Cookie("memberId", 
-					String.valueOf(loginMember.getId()));
+		Cookie idCookie = new Cookie("memberId",
+				String.valueOf(loginMember.getId()));
 		response.addCookie(idCookie);
 		reAttributes.addFlashAttribute("msg", "로그인 성공");
 		return "redirect:/";
 	}
-	
-	@PostMapping("/login")
+
+	//	@PostMapping("/login")
 	public String loginv3(@ModelAttribute LoginForm form,
-			Model model, RedirectAttributes redirectAttributes,
-			HttpServletRequest request) {
-		Member loginMember 
-		= loginService.login(form.getLoginId(), form.getPassword());
-		if( loginMember == null ) {
+						  Model model, RedirectAttributes redirectAttributes,
+						  HttpServletRequest request) {
+		Member loginMember
+				= loginService.login(form.getLoginId(), form.getPassword());
+		if (loginMember == null) {
 			// 로그인 실패
 			model.addAttribute("msg", "로그인 실패");
 			return "login/loginForm";
 		}
-		
 		// 로그인 성공 처리
 		// 세션이 있으면 세션 반환, 없으면 신규 세션 생성
 		HttpSession session = request.getSession();
@@ -74,13 +73,28 @@ public class LoginController {
 		session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
 		return "redirect:/";
 	}
-	
-	
-	
-	
-	
-	
-	
+
+	@PostMapping("/login")
+	public String loginv4(@ModelAttribute LoginForm form,
+			Model model, RedirectAttributes redirectAttributes,
+			HttpServletRequest request,
+			@RequestParam(defaultValue = "/")String redirectURL) {
+		Member loginMember
+		= loginService.login(form.getLoginId(), form.getPassword());
+		if( loginMember == null ) {
+			// 로그인 실패
+			model.addAttribute("msg", "로그인 실패");
+			return "login/loginForm";
+		}
+
+		// 로그인 성공 처리
+		// 세션이 있으면 세션 반환, 없으면 신규 세션 생성
+		HttpSession session = request.getSession();
+		// 세션에 로그인 회원 정보를 보관
+		session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+		return "redirect:" + redirectURL;
+	}
+
 	// setMaxAge(0) : 쿠키 즉시 종료
 //	@PostMapping("/logout")
 	public String logoutv2(HttpServletResponse response) {
